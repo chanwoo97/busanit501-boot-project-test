68b0ac1f84928c2c34043a75ed0f28374ea233a0
이상용 <lsy3709@naver.com>
2025-07-07 14:09:29 +0900

    5교시, 타임리프 기본 문법 확인 예시1

--- src/main/java/com/busanit501/boot_project/controller/SampleController.java
+++ src/main/java/com/busanit501/boot_project/controller/SampleController.java
@@ -5,6 +5,9 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Arrays;
+import java.util.List;
+
 @Controller
 @Log4j2
 public class SampleController {
@@ -13,6 +16,14 @@ public class SampleController {
     public void hello(Model model) {
         log.info("SampleController에서,/hello:  hello");
         model.addAttribute("msg", "Hello World!");
-
     }
-}
+
+    @GetMapping("/ex/ex1")
+    public void ex1(Model model) {
+        // 화면에 전달할 샘플 데이터 배열
+        List<String> list = Arrays.asList("aaa","bbb","ccc","ddd");
+        // 서버 -> 화면, 데이터 전달.
+        model.addAttribute("list", list);
+    }
+    
+} //SampleController 닫는 블록
