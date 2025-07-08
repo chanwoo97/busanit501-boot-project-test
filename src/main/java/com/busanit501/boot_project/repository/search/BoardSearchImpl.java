61dbb948a614097ea64b2775a416d9f9df0b88d0
이상용 <lsy3709@naver.com>
2025-07-08 15:21:58 +0900

    6교시, Querydsl 사용해서, query  전달, 페이징 조건 + 검색 조건 추가 전달 searchAll

--- src/main/java/com/busanit501/boot_project/repository/search/BoardSearchImpl.java
+++ src/main/java/com/busanit501/boot_project/repository/search/BoardSearchImpl.java
@@ -2,6 +2,7 @@ package com.busanit501.boot_project.repository.search;
 
 import com.busanit501.boot_project.domain.Board;
 import com.busanit501.boot_project.domain.QBoard;
+import com.querydsl.core.BooleanBuilder;
 import com.querydsl.jpa.JPQLQuery;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -45,6 +46,51 @@ public class BoardSearchImpl extends QuerydslRepositorySupport implements BoardS
         // 순서8
         // 페이징 조건 적용 + 전체 갯수
         long count2 = query.fetchCount();
+
+        return null;
+    }
+
+    // 페이징 정보 + 검색 정보를 이용해서,
+    // 자바 메서드로 -> sql 전달하기.
+    @Override
+    public Page<Board> searchAll(String[] types, String keyword, Pageable pageable) {
+        // 순서1
+        QBoard board = QBoard.board; // (board)
+        // 순서2
+        JPQLQuery<Board> query = from(board); // select .. from board
+        // 순서3,
+        // where 조건절 , BooleanBuilder 를 이용해서 조건 추가.
+        // select .. from board where ....
+        if ((types != null && types.length > 0) && keyword != null) {
+            // or , 조건, and 조건을 사용하기 싶다. 묶기도 쉽다.
+            BooleanBuilder builder = new BooleanBuilder();
+            // types = {"t","w","c"}
+            for(String type : types){
+                switch (type) {
+                    case "t":
+                        builder.or(board.title.contains(keyword));
+                        break;
+                    case "c":
+                        builder.or(board.content.contains(keyword));
+                        break;
+                    case "w":
+                        builder.or(board.writer.contains(keyword));
+                        break;
+                } // end switch
+            } // end for
+            query.where(builder); // select * from board where like %keyword%
+        } //end if
+        // bno >0 조건 추가히기.
+        query.where(board.bno.gt(0L));
+
+        // paging 조건 추가하기.
+        // 순서4,
+        this.getQuerydsl().applyPagination(pageable,query);
+        // 순서5,
+        // 위의 검색 조건, 페이징 조건으로 sql  전달하기.  데이터 가져오기
+        List<Board> list = query.fetch();
+        // 순서6,
+        long count = query.fetchCount();
 
         return null;
     }
