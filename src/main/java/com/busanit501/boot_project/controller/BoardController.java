8e4d6878355d03f0105734ce7885f7110b86ac3a
이상용 <lsy3709@naver.com>
2025-07-10 14:22:31 +0900

    5교시, 화면 템플릿 basic.html 이용해서, list.html 기본 구성하기2

--- src/main/java/com/busanit501/boot_project/controller/BoardController.java
+++ src/main/java/com/busanit501/boot_project/controller/BoardController.java
@@ -1,8 +1,11 @@
 package com.busanit501.boot_project.controller;
 
+import com.busanit501.boot_project.dto.BoardDTO;
 import com.busanit501.boot_project.dto.PageRequestDTO;
+import com.busanit501.boot_project.dto.PageResponseDTO;
 import com.busanit501.boot_project.service.BoardService;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,12 +14,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/board")
 @RequiredArgsConstructor
+@Log4j2
 public class BoardController {
     private final BoardService boardService;
 
     @GetMapping("/list")
     public void list(PageRequestDTO pageRequestDTO, Model model) {
-
+        // 서비스 외주 이용해서, 데이터 가져오기
+        PageResponseDTO<BoardDTO> responseDTO = boardService.list(pageRequestDTO);
+        log.info("BoardController에서, list, responseDTO : {}", responseDTO);
+        // 서버 -> 화면으로 데이터 전달.
+        model.addAttribute("responseDTO", responseDTO);
     }
 
 }
