cc09bd29d0a57bddd161deb7e7dbe91e1da48f39
이상용 <lsy3709@naver.com>
2025-07-11 16:36:31 +0900

    7교시, RESTful 댓글 작업 개요4, 레스트 작업시, 데이터의 유효성을 확인 하기 어려움, 그래서, 서버 쪽에서, 유효성 체크 설정 하기.적용해보기

--- src/main/java/com/busanit501/boot_project/controller/ReplyController.java
+++ src/main/java/com/busanit501/boot_project/controller/ReplyController.java
@@ -1,10 +1,13 @@
 package com.busanit501.boot_project.controller;
 
 import com.busanit501.boot_project.dto.ReplyDTO;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindException;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -40,8 +43,15 @@ public class ReplyController {
     // 전달 내용.
     // 1) http status code , 200 ok, 2) 데이터 내용
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
-    public ResponseEntity<Map<String,Long>> register(@RequestBody ReplyDTO replyDTO) {
+    public ResponseEntity<Map<String,Long>> register( @Valid @RequestBody ReplyDTO replyDTO,
+    BindingResult bindingResult) throws BindException
+    {
         log.info("ReplyController에서 작업중, 댓글 작성작업");
+
+        if(bindingResult.hasErrors()) {
+            throw new BindException(bindingResult);
+        }
+
         log.info("replyDTO : " + replyDTO);
         // 화면이 없어서, 하드코딩으로 더미 데이터 만들기.
         Map<String,Long> resultMap = Map.of("rno",100L);
