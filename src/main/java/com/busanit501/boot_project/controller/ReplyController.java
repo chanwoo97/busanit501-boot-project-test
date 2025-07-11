f74151d8512a6735f70d7111c328bcc85107a3da
이상용 <lsy3709@naver.com>
2025-07-11 16:08:46 +0900

    7교시, RESTful 댓글 작업 개요3, 스웨거 , 포스트맨이라는 도구 이용해서, 레스트 컨트롤러 테스트,

--- src/main/java/com/busanit501/boot_project/controller/ReplyController.java
+++ src/main/java/com/busanit501/boot_project/controller/ReplyController.java
@@ -18,7 +18,27 @@ import java.util.Map;
 @Log4j2
 @RequiredArgsConstructor
 public class ReplyController {
+    // JSON 데이터 타입의 기본 모양, { "키" : 값, "키2":값2,... }
+    // MIME 타입, -> MediaType.APPLICATION_JSON_VALUE
+    //{
+    //  "rno": 0,
+    //  "bno": 0,
+    //  "replyText": "string",
+    //  "replyer": "string",
+    //  "regDate": "2025-07-11T06:38:49.331Z",
+    //  "modDate": "2025-07-11T06:38:49.331Z"
+    //}
 
+    // 네트워크 통해서, 실제로 전달시에는 문자열 형태로 전달.
+
+    // @RequestBody ReplyDTO replyDTO
+    // 의미 : 화면에서, JSON 데이터(문자열) 전달함
+    // -> 서버에서, 객체 형태로 변경해줌. 역직렬화라고 부름.
+
+    //ResponseEntity<Map<String,Long>>
+    // 의미 : 서버 -> 화면에게 , 응답하는 타입,
+    // 전달 내용.
+    // 1) http status code , 200 ok, 2) 데이터 내용
     @PostMapping(value = "/", consumes = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<Map<String,Long>> register(@RequestBody ReplyDTO replyDTO) {
         log.info("ReplyController에서 작업중, 댓글 작성작업");
