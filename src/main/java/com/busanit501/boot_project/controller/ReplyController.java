d4fe29f5b3db909381faba3c61eb0efbe908633f
이상용 <lsy3709@naver.com>
2025-07-15 10:27:58 +0900

    2교시, 게시글에 대한 , 댓글 삭제, 레스트 컨트롤러 작업

--- src/main/java/com/busanit501/boot_project/controller/ReplyController.java
+++ src/main/java/com/busanit501/boot_project/controller/ReplyController.java
@@ -72,7 +72,7 @@ public class ReplyController {
     @Tag(name = "댓글 목록 조회", description = "댓글 목록 조회 레스트 버전 get 방식")
     @GetMapping(value = "/list/{bno}")
     public PageResponseDTO<ReplyDTO> getList(@PathVariable Long bno, PageRequestDTO pageRequestDTO) {
-        log.info("ReplyController에서 작업중, 전달받은 bno 확인 :  " + bno);
+        log.info("ReplyController에서 작업중, 댓글 목록 조회, 전달받은 bno 확인 :  " + bno);
 
         PageResponseDTO<ReplyDTO> responseDTO = replyService.getListOfBoard(bno, pageRequestDTO);
         return responseDTO;
@@ -84,10 +84,25 @@ public class ReplyController {
     @Tag(name = "댓글 하나 조회", description = "댓글 하나 조회 레스트 버전 get 방식")
     @GetMapping(value = "/{rno}")
     public ReplyDTO getReplyDTO(@PathVariable Long rno) {
-        log.info("ReplyController에서 작업중, 전달받은 rno 확인 :  " + rno);
+        log.info("ReplyController에서 작업중,하나 조회 전달받은 rno 확인 :  " + rno);
 
         ReplyDTO replyDTO = replyService.read(rno);
         return replyDTO;
+
+    }
+
+    // 댓글 삭제
+    // http://localhost:8080/replies/612
+    @Tag(name = "댓글 삭제", description = "댓글 삭제 레스트 버전 delete 방식")
+    @DeleteMapping(value = "/{rno}")
+    public Map<String,Long> remove(@PathVariable Long rno) {
+        log.info("ReplyController에서 작업중, 삭제, 전달받은 rno 확인 :  " + rno);
+
+        replyService.remove(rno);
+        Map<String,Long> resultMap = new HashMap<>();
+        resultMap.put("rno",rno);
+
+        return resultMap;
 
     }
 
