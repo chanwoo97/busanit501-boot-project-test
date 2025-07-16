b76fe2e673be8b15be2db51c3c02bff255275d19
이상용 <lsy3709@naver.com>
2025-07-16 10:49:19 +0900

    2교시, 파일 업로드 작업 준비 설정 2, 업로드 파일명 확인 예시

--- src/main/java/com/busanit501/boot_project/controller/UpDownController.java
+++ src/main/java/com/busanit501/boot_project/controller/UpDownController.java
@@ -2,7 +2,9 @@ package com.busanit501.boot_project.controller;
 
 import com.busanit501.boot_project.dto.upload.UploadFileDTO;
 import io.swagger.v3.oas.annotations.tags.Tag;
+
 import lombok.extern.log4j.Log4j2;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,10 +13,21 @@ import org.springframework.web.bind.annotation.RestController;
 @Log4j2
 public class UpDownController {
 
+    // 업로드 할 , 저장 위치를 불러오기. com.busanit501.upload.path
+    @Value("${com.busanit501.upload.path}") // 스프링에서 지원해주는 패키지 경로 사용하기.
+    private String uploadPath;
+
     @Tag(name = "파일등록" ,  description = "멀티파트 타입 형식으로, 업로드 테스트, post 방식")
     @PostMapping(value = "/upload", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
     public String upload(UploadFileDTO uploadFileDTO) {
         log.info("UpDownController에서 작업중, upload메소드에서, uploadFileDTO 확인 : " + uploadFileDTO);
+
+        if(uploadFileDTO.getFiles() != null && uploadFileDTO.getFiles().size() > 0) {
+            uploadFileDTO.getFiles().forEach(multipartFile -> {
+                log.info("업로드한 파일명 : "+multipartFile.getOriginalFilename());
+            });
+        }
+
         return null;
     }
 
