3bdb58cdcffe19aab379682536e984287cfdc184
이상용 <lsy3709@naver.com>
2025-07-23 11:24:04 +0900

    3교시, 커스텀 로그인 페이지 , 컨트롤러 작업 하기.

---
+++ src/main/java/com/busanit501/boot_project/controller/MemberController.java
@@ -0,0 +1,20 @@
+package com.busanit501.boot_project.controller;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/member")
+@Log4j2
+@RequiredArgsConstructor
+public class MemberController {
+
+    @GetMapping("/login")
+    public void loginGet(String error, String logout) {
+        log.info("====커스텀 로그인 페이지 화면 get===============================");
+        log.info("logout : " + logout);
+    }
+}
