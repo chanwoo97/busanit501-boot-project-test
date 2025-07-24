ff37d0eb84e74e58aaad332ca82dfe14ff636634
이상용 <lsy3709@naver.com>
2025-07-24 09:40:21 +0900

    0724, 1교시, 복습, Remember Me 설정 테이블 적용 및 개념

--- src/main/java/com/busanit501/boot_project/security/CustomUserDetailsService.java
+++ src/main/java/com/busanit501/boot_project/security/CustomUserDetailsService.java
@@ -12,7 +12,16 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 import java.util.stream.Collectors;
+// 시큐리티에서는 로그인시, 원하는 포맷 형태가 있음.
+// 시큐리티가 만들어 둔 UserDetails 라는 타입으로 맞춰주기.
+// 시큐리티에서 제공하는 UserDetailsService 라는 인터페이스 구현하면
+// 좀 더 쉽게 , 시큐리티가 원하는 타입을 만들수 있음.
 
+// 업무
+    // 시큐리티에 폼방식으로 접근하는 유저명을 확인하고, 디비에 저장된 유저가 맞다면,
+    // 접근 할수 있는 타입을 전달해줌.
+    // 화면에서 전달받은 인증 , 아이디 와
+    // 디비에 있는 아이디의 일치 여부를 검사하는 직원.
 @Log4j2
 @Service
 //@RequiredArgsConstructor
