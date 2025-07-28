f94bc454e15e1aa8d54e3fa21f61d8ad50bc8e07
이상용 <lsy3709@naver.com>
2025-07-28 15:05:44 +0900

    6교시, 카카오 로그인 연동 작업4, modify 더미 페이지 확인3

--- src/main/java/com/busanit501/boot_project/security/handler/CustomSocialLoginSuccessHandler.java
+++ src/main/java/com/busanit501/boot_project/security/handler/CustomSocialLoginSuccessHandler.java
@@ -29,7 +29,7 @@ public class CustomSocialLoginSuccessHandler implements AuthenticationSuccessHan
         String encodedPw = memberSecurityDTO.getMpw();
 
         // 임의로 소셜 로그인시, 회원 패스워드 임의로 1111 로 지정함.
-        if(memberSecurityDTO.isSocial() && (memberSecurityDTO.getMpw().equals("1111"))
+        if(memberSecurityDTO.isSocial() && (passwordEncoder.equals("1111"))
         || passwordEncoder.matches("1111",memberSecurityDTO.getMpw())){
             log.info("소셜로 로그인시 임시 비밀번호 1111 를 그대로 사용한 경우");
             // 임시, 회원 수정 페이지.
