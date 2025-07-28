37b09332d95432324a0b5ed9987056fe202288c9
이상용 <lsy3709@naver.com>
2025-07-28 11:34:26 +0900

    3교시, 카카오 로그인 연동 작업2, 카카오 인증 서버로 인증 후, 로그인한 유저 정보 1차 확인

---
+++ src/main/java/com/busanit501/boot_project/security/CustomOAuth2UserService.java
@@ -0,0 +1,21 @@
+package com.busanit501.boot_project.security;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Log4j2
+@Service
+@RequiredArgsConstructor
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        log.info("CustomOAuth2UserService에서, 인증한 유저 정보 확인 userRequest : "  + userRequest);
+        return super.loadUser(userRequest);
+    }
+}
